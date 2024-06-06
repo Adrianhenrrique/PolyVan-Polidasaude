e10a19ce25e9388520954faf5c3aa7acc0d7b493
Adrian Henrique <113368090+Adrianhenrrique@users.noreply.github.com>
2024-06-05 21:55:35 -0300

    create information extra section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Banner from './components/banner/Banner';
 import Header from './components/header/Header';
+import InformacaoExtra from './components/informacaoExtra/informacaoExtra';
+import TestFirebase from './components/testeFirebase';
 
 function App() {
   return (
     <>
       <Header/>
       <Banner/>
+      <InformacaoExtra/>
     </>
   );
 }
---
+++ src/components/informacaoExtra/informacaoExtra.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from './informacaoExtra.module.css';
+import checkImage from '../../assets/imgs/checkImags.svg';
+import clockImage from '../../assets/imgs/clock.svg';
+
+function InformacaoExtra() {
+    return (
+        <section className={style.informacaoExtra}> 
+            <ul className={style.informacaoExtra__lista}>
+                <li className={style.informacaoExtra__lista__item}>
+                    <img className={style.informacaoExtra__lista__item__icon} src={checkImage} alt="check" />
+                    <div className={style.informacaoExtra__lista__item__container}>
+                        <h2 className={style.informacaoExtra__lista__item__container__titulo}>+3 anos de experiência</h2>
+                        <p className={style.informacaoExtra__lista__item__container__text}>
+                            Cuidando da sua saúde com excelência e dedicação há mais de 3 anos.
+                        </p>
+                    </div>
+                </li>
+                <li className={style.informacaoExtra__lista__item}>
+                    <img className={style.informacaoExtra__lista__item__icon} src={clockImage} alt="clock" />
+                    <div className={style.informacaoExtra__lista__item__container}>
+                        <h2 className={style.informacaoExtra__lista__item__container__titulo}>Atendimento superior</h2>
+                        <p className={style.informacaoExtra__lista__item__container__text}>
+                            Oferecendo atendimento superior e cuidado dedicado à sua saúde todos os dias.
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    );
+}
+
+export default InformacaoExtra;
