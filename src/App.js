0eadbcb385bdc87cfb069d382e3fc59a548685f2
Adrian Henrique <113368090+Adrianhenrrique@users.noreply.github.com>
2024-06-05 23:43:54 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from './components/about/about';
 import Banner from './components/banner/Banner';
 import Header from './components/header/Header';
 import InformacaoExtra from './components/informacaoExtra/informacaoExtra';
-import TestFirebase from './components/testeFirebase';
 
 function App() {
   return (
